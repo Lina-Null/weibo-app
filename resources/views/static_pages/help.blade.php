b5e6daaf94bb848cfde54db23cd246b90f16f3ba
Lina-Null <lina0709521@icloud.com>
2022-03-03 15:01:44 +0800

    静态基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,9 @@
+<!DOTYPE html>
+<html>
+    <head>
+        <title>Weibo App</title>
+    </head>
+    <body>
+        <h1>帮助页</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPage\StaticPageController@home');
+Route::get('/help', 'StaticPage\StaticPageController@help');
+Route::get('/about', 'StaticPage\StaticPageController@about');
