bcdac0e2854e9b62eccbcc3c6a9e970c305d77d1
Lina-Null <lina0709521@icloud.com>
2022-03-06 21:04:52 +0800

    home页基本静态页框架

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,127 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title','Weibo App') - Laravel菜鸟</title>
+    <head >
+        <title>@yield('title','Weibo App') - 自由职业之路</title>
+        <link rel="stylesheet" href="/css/app.css">
+       
     </head>
     <body>
-        @yield('content')
+  
+ 
+        
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark bd-navbar  fixed-top nav-border">
+            <div class="container">
+                <a href="#" class="navbar-brand text-golden">我在Weibo-App学习</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- <div class="collapse navbar-collapse" id="navmenu"> -->
+                    <!-- ms-auto   margin start =margin left  -->
+                    <!-- <ul class="navbar-nav ms-auto"> 
+                        <li class="nav-item"><div class="nav-link">前端知识</div></li>
+                        <li class="nav-item"><div class="nav-link">后端知识</div></li>
+                        <li class="nav-item"><div class="nav-link">数据库知识</div></li>
+                    </ul>
+                </div> -->
+                <div class="collapse navbar-collapse " id="navmenu" >
+                    <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav pt-2 py-md-0">
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2 active" href="#" >前端知识</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2 " aria-current="true" href="#" >后端知识</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >大数据</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >人工智能</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >测试</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >爬虫</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >书籍</a>
+                      </li>
+                      <li class="nav-item  col-md-auto">
+                        <a class="nav-link p-2" href="#" >工具</a>
+                      </li>
+                    
+                    </ul>
+              
+                    <hr class="d-md-none text-white-50">
+              
+                    <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+                      <li class="nav-item col-6 col-md-auto">
+                            <a class="nav-link p-2 d-flex" href="https://github.com/twbs" target="_blank" rel="noopener">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" class="navbar-nav-svg d-inline-block align-text-top" viewBox="0 0 512 499.36" role="img"><title>GitHub</title><path fill="currentColor" fill-rule="evenodd" d="M256 0C114.64 0 0 114.61 0 256c0 113.09 73.34 209 175.08 242.9 12.8 2.35 17.47-5.56 17.47-12.34 0-6.08-.22-22.18-.35-43.54-71.2 15.49-86.2-34.34-86.2-34.34-11.64-29.57-28.42-37.45-28.42-37.45-23.27-15.84 1.73-15.55 1.73-15.55 25.69 1.81 39.21 26.38 39.21 26.38 22.84 39.12 59.92 27.82 74.5 21.27 2.33-16.54 8.94-27.82 16.25-34.22-56.84-6.43-116.6-28.43-116.6-126.49 0-27.95 10-50.8 26.35-68.69-2.63-6.48-11.42-32.5 2.51-67.75 0 0 21.49-6.88 70.4 26.24a242.65 242.65 0 0 1 128.18 0c48.87-33.13 70.33-26.24 70.33-26.24 14 35.25 5.18 61.27 2.55 67.75 16.41 17.9 26.31 40.75 26.31 68.69 0 98.35-59.85 120-116.88 126.32 9.19 7.9 17.38 23.53 17.38 47.41 0 34.22-.31 61.83-.31 70.23 0 6.85 4.61 14.81 17.6 12.31C438.72 464.97 512 369.08 512 256.02 512 114.62 397.37 0 256 0z"></path></svg>
+                                    <!-- <small class="d-md-none ms-2">GitHub</small> --> 
+                                <span class=" text-start text-center text-warning">66666</span>
+                            </a>
+                           
+                      </li>
+ 
+                    </ul>
+              
+                    <a class="btn btn-bd-download d-lg-inline-block my-2 my-md-0 ms-md-2" href="#">注册</a>
+                    <a class="btn btn-bd-download d-lg-inline-block my-2 my-md-0 ms-md-2" href="#">登录</a>
+                  </div>
+            </div>
+        </nav>
+        <section >
+           
+                @yield('content')
+            </div>
+          </section> 
+        <section class="p-5 intro-section text-success ">
+            <div class="container">
+           
+                <div class="row text-white justify-content-center mt-3 pb-3">
+                    <div class="col-12 col-sm-6 col-lg-6 mx-auto px-4 text-md-start text-sm-center">
+                        <h5 class="fw-bold ls-3">共同进步</h5>
+                        <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                        <p class="lh-lg fs-17 ls-1">
+                            不要相信任何人或者机构能把你培训成高手，更重要的是自己的努力和坚持。 
+                          </p>
+                            <p>做一个理智的技术人，不要陷入到技术陷阱中。</p>
+                           
+                       
+                    </div>
+                    <div class="col-12 col-sm-6 col-lg-3 mb-4 mx-auto text-center">
+                        <h5 class="fw-bold ls-3">我的作品</h5>
+                        <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                        <ul class="list-inline campany-list">
+                            <li>
+                                <a class="text-light" href="http://www.weibo-App.test" target="blank">
+                                   我在Weibo-App学习
+                                </a>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-6 col-lg-3 mb-4 mx-auto text-center">
+                        <h5 class="fw-bold text-center ls-3">我的微信</h5>
+                        <div>
+                            <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                        </div>
+                        <img src="" alt="" class="img-fluid rounded-10" style="max-width: 240px !important;">
+                    </div>
+                </div>
+          
+            </div>
+
+     
+
+        </section>
+        <footer class="p-5 bg-dark text-light text-center">
+            <div class="container">
+                <p class="lead">
+                    Copyright &copy; 2022 <span class="text-golden">我在weibo-App学习</span>
+                </p>
+            </div>
+        </footer>
     </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,355 @@
-@extends('layouts.default')
+  @extends('layouts.default')
 
-@section('content')
-  <h1>主页</h1>
-@stop
+  @section('content')
+  <section  class="p-5 bg-dark text-light text-center text-sm-start">
+  <div class="container ">
+    <div class="d-flex ">
+      <div>
+        <h1>成为一名<span class="text-golden">自由职业者</span></h1>
+        <p class="my-4 display-2 ">嗨! 朋友 </p>
+        <p class="my-4 display-6">让我们一起努力，争取成为一名有竞争力的<b>“</b>自由职业者<b>”</b></p>
+        <button class="btn btn-danger btn-lg">开启编程之旅</button>
+      </div>
+      <!--  -->
+      <img src="" alt="" class="w-50 d-none d-md-block">
+    </div>
+  </div>
+  </section>
+
+
+  <section class="p-5  text-light intro-section">
+    <div class="container ">
+      <!-- <div class="d-md-flex justify-content-between align-items-center "> -->
+        <div class="topBox   align-items-center  d-md-flex justify-content-between row gx-5">
+            <div class="topBox-left col-7 col-md-7  col-sm-7 col-xs-7">
+              <div class=" div-bannerTips bd-golden  ">
+                <i class="bi bi-robot text-info"></i>
+                <span >
+                  最新发布
+                </span>
+                <b>|</b>
+                <span class="text-info">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+              </div>
+              
+            </div>
+            <div class="topBox-right col-5 col-md-5 col-md-5  col-sm-5 col-xs-5 text-center align-items-center">
+              <ul class="topBox-right-ul row text-center list-unstyled ">
+                <li class="col text-center d-flex justify-content-evenly align-items-center">
+                  
+                    <div class="div-icon bg-danger"><i class="bi bi-cloud-arrow-up fs-4 "></i></div>
+                    <div class="d-flex flex-column">
+                      <span class="p-1 bd-highlight">今日发布</span>
+                      <span class="p-1 bd-highlight">1</span>
+                    </div>
+                  
+                </li >
+                <li class="col text-center d-flex justify-content-evenly align-items-center">
+                
+                    <div class="div-icon bg-golden"><i class="bi bi-gem fs-4 "></i></div>
+                    <div class=" d-flex flex-column">
+                      <span class="p-1 bd-highlight">访客总数</span>
+                      <span class="p-1 bd-highlight">6666</span>
+                    </div>
+                 
+                </li>
+                <li class="col text-center d-flex justify-content-evenly align-items-center">
+                  
+                    <div class="div-icon bg-info"><i class="bi bi-stack fs-4 "></i></div>
+                    <div class="d-flex flex-column">
+                      <span class="p-1 bd-highlight">资源总数</span>
+                      <span class="p-1 bd-highlight">888888</span>
+                    </div>
+                 
+                </li>
+              </ul>
+            </div>
+        </div>
+ 
+      <!-- </div> -->
+    </div>
+  </section>
+
+  <section class="p-5 bg-dark ">
+    <div class="container">
+      <div class="row home-news pb-5">
+          <h1 class="text-center">
+            <span class="home-news-dot">·</span>
+            <span class="home-news-font">NEWS </span>
+            <span class="home-news-new">
+                <strong>最新</strong>
+                <strong>发布</strong>
+            </span>
+            <span class="home-news-dot">·</span>
+          </h1>
+      </div>
+      <div class="row g-4">
+        <div class="col-md">
+          <div class="card  bg-dark text-light text-center home-card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light text-center home-card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">后端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light text-center home-card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">大数据知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >Html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">后端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-python language-color"></i>
+                <span >Python</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">后端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-php language-color"></i>
+                <span >PHP</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">后端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-java language-color"></i>
+                <span >Java</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-javascript language-color"></i>
+                <span >JavaScript</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card  bg-dark text-light home-card text-center" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title my-3">前端知识</h5>
+             
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-desc my-3">
+                <i class="type-html language-color"></i>
+                <span >html</span>
+              </p>
+              <a href="#" class="btn btn-danger justify-content-center">Card link</a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+  </section>
+
+  @stop
