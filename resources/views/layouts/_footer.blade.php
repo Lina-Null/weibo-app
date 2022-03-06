c789a270d12960c99e01f3e819d39e633a09a1e3
Lina-Null <lina0709521@icloud.com>
2022-03-06 21:58:43 +0800

    分割default页面：添加头部、尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,47 @@
+<section class="p-5 intro-section text-success ">
+    <div class="container">
+   
+        <div class="row text-white justify-content-center mt-3 pb-3">
+            <div class="col-12 col-sm-6 col-lg-6 mx-auto px-4 text-md-start text-sm-center">
+                <h5 class="fw-bold ls-3">共同进步</h5>
+                <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                <p class="lh-lg fs-17 ls-1">
+                    不要相信任何人或者机构能把你培训成高手，更重要的是自己的努力和坚持。 
+                  </p>
+                    <p>做一个理智的技术人，不要陷入到技术陷阱中。</p>
+                   
+               
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 mb-4 mx-auto text-center">
+                <h5 class="fw-bold ls-3">我的作品</h5>
+                <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                <ul class="list-inline campany-list">
+                    <li>
+                        <a class="text-light" href="http://www.weibo-App.test" target="blank">
+                           我在Weibo-App学习
+                        </a>
+                    </li>
+                   
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 mb-4 mx-auto text-center">
+                <h5 class="fw-bold text-center ls-3">我的微信</h5>
+                <div>
+                    <hr class="bg-white d-inline-block mb-4" style="width: 100px; height: 2px;">
+                </div>
+                <img src="" alt="" class="img-fluid rounded-10" style="max-width: 240px !important;">
+            </div>
+        </div>
+  
+    </div>
+
+
+
+</section>
+<footer class="p-5 bg-dark text-light text-center">
+    <div class="container">
+        <p class="lead">
+            Copyright &copy; 2022 <span class="text-golden">我在weibo-App学习</span>
+        </p>
+    </div>
+</footer>
