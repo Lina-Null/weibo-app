3cf0afdc143f94dfa074963c24c688dfb85a72a6
Lina-Null <lina0709521@icloud.com>
2022-03-11 08:56:47 +0800

    用户注册表单

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -11,9 +11,37 @@
 
     @include('register._header')
         
-       <section class="p-5 bg-dark text-light text-center">
+       <section class="p-5 bg-dark  text-center">
            <div class="container ">
-               <h1>注册页</h1>
+               <div class="offset-md-3 col-md-6 text-center">
+                   <div class="card">
+                       <div class="card-header">
+                           <h5 >注册</h5>
+                       </div>
+                       <div class="card-body">
+                        <form method="POST" action="{{ route('users.store') }}">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">名称</span>
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                              </div>
+                  
+                              <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">邮箱</span>
+                                <input type="text" class="form-control" placeholder="xxxxx@xxx.xxx" aria-label="email" aria-describedby="basic-addon1">
+                              </div>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">密码</span>
+                                <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                              </div>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">密码</span>
+                                <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                              </div>
+                            <button type="submit" class="btn btn-bd-download-active ">注册</button>
+                       </div>
+                        
+                    </div>
+               </div>
            </div>
        </section>
       
